65cb8921ec7b65ace4aa02461872ab44d838be9d
shinis(ysit) <actionshin@gmail.com>
2025-01-16 14:36:12 +0900

    Model로 데이터 전달해서 상세보기 구현

---
+++ src/main/java/com/myfruit/pms/controller/ItemController.java
@@ -0,0 +1,26 @@
+package com.myfruit.pms.controller;
+
+import com.myfruit.pms.dto.ItemDto;
+import com.myfruit.pms.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/items")
+public class ItemController {
+
+    @Autowired
+    ItemService itemService;
+
+    @GetMapping("/{id}")
+    public String getItem(@PathVariable("id") int id, Model model) {
+        ItemDto itemDto = itemService.getItem(id);
+        model.addAttribute("item", itemDto);
+
+        return "shop/detail";
+    }
+}
