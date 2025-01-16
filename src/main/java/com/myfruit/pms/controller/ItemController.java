0e2d538eb2a7484ac1692044b3649df489f5116a
shinis(ysit) <actionshin@gmail.com>
2025-01-16 14:05:37 +0900

    아이템 추가 구현

--- src/main/java/com/myfruit/pms/controller/ItemController.java
+++ src/main/java/com/myfruit/pms/controller/ItemController.java
@@ -1,12 +1,17 @@
 package com.myfruit.pms.controller;
 
 import com.myfruit.pms.dto.ItemDto;
+import com.myfruit.pms.mapper.ItemMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/items")
-public class ItemClass {
+public class ItemController {
+
+    @Autowired
+    private ItemMapper itemMapper;
 
     @GetMapping("/create")
     public String create() {
@@ -14,8 +19,10 @@ public class ItemClass {
     }
 
     @PostMapping
+    @ResponseBody
     public void createItem(@RequestBody ItemDto itemDto) {
         System.out.println(itemDto.getItem());
+        itemMapper.insertItem(itemDto);
     }
 
 }
