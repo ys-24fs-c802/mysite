ad4a1146fd26903b66c47456415345091a7ed723
shinis(home) <actionshin@gmail.com>
2025-01-18 06:17:34 +0900

    등록 후 id(pk) 확인(ItemDto에 반영)

--- src/main/java/com/myfruit/pms/mapper/ItemMapper.java
+++ src/main/java/com/myfruit/pms/mapper/ItemMapper.java
@@ -5,5 +5,6 @@ import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface ItemMapper {
-    void insertItem(ItemDto itemDto);
+    // ItemDto insertItem(ItemDto itemDto); // PK가 생성, 전달된 객체와 같은 참조를 가지고 있어 정보 추가
+    int insertItem(ItemDto itemDto); // PK가 생성, int는 성공 또는 실패 반환
 }
