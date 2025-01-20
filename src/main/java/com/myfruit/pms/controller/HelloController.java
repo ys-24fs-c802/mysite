d59d964a095947c387906cb5c877dd508ed0423d
shinis(ysit) <actionshin@gmail.com>
2025-01-20 12:44:46 +0900

    홈페이지 추가

--- src/main/java/com/myfruit/pms/controller/HelloController.java
+++ src/main/java/com/myfruit/pms/controller/HelloController.java
@@ -10,4 +10,9 @@ public class HelloController {
     public String hello() {
         return "common/hello";
     }
+
+    @GetMapping("/")
+    public String home() {
+        return "common/home";
+    }
 }
