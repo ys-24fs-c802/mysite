57a93cc43917bc1f4c8ff9ea1b1e758474b30891
shinis(ysit) <actionshin@gmail.com>
2025-01-21 12:21:11 +0900

    입력폼 유효성 검사 구현

--- src/main/resources/static/js/items.js
+++ src/main/resources/static/js/items.js
@@ -1,9 +1,47 @@
+// 공백 검사
+function hasWhiteSpace(str) {
+    return /\s/.test(str)
+}
+
+// 특수문자 검사 [!@#$%]
+// ., -, \는 정규시에 사용되는 문자이므로 이스케이프(escape) 처리를 해야 합니다.
+function hasSpecialChar(str) {
+    return /[!@#$%^&*():{}|<>,'~_=\.\-\\`]/.test(str);
+}
+
+// 숫자로 시작하는지 검사
+function startWithNumber(str) {
+    // 1abc, 9abc 시작할 때 검사는 ^를 사용합니다.
+    return /^[0-9]/.test(str)
+}
+
+document.getElementById('item_id').addEventListener('input', function(e) {
+    const value = e.target.value;
+    const spaceError = document.getElementById('spaceError');
+    const specialCharError = document.getElementById('specialCharError');
+    const startWithNumberError = document.getElementById('startWithNumberError');
+
+    //console.log(value, "=>", hasWhiteSpace(value));
+
+    spaceError.style.display = hasWhiteSpace(value) ? 'block' : 'none';
+    specialCharError.style.display = hasSpecialChar(value) ? 'block' : 'none';
+    startWithNumberError.style.display = startWithNumber(value) ? 'block' : 'none';
+})
+
 document.getElementById('itemForm').addEventListener('submit', function(e) {
 
     e.preventDefault();
 
     const item = {
         item: document.getElementById('item_id').value,
+    }
+
+    if (! hasWhiteSpace(item.item) &&
+        ! hasSpecialChar(item.item) &&
+        ! startWithNumber(item.item)) {
+            alert('서버로 전송한다.');
+    } else {
+        alert('입력값을 다시 확인해주세요.');
     }
 
     // fetch(요청주소, 요청내용객체)
