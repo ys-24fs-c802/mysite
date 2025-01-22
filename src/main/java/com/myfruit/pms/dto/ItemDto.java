3c02c9f59b8e696823134a24438665ebdf822e83
shinis(home) <actionshin@gmail.com>
2025-01-22 23:13:24 +0900

    ItemDto에 대한 유효성 검사 추가

--- src/main/java/com/myfruit/pms/dto/ItemDto.java
+++ src/main/java/com/myfruit/pms/dto/ItemDto.java
@@ -1,22 +1,21 @@
 package com.myfruit.pms.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ItemDto {
     private Integer id;
-    private String item;
 
-    // Getter와 Setter 설정
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-    public String getItem() {
-        return item;
-    }
-    public void setItem(String item) {
-        this.item = item;
-    }
+    @NotBlank(message = "아이템은 필수입니다")
+    @Pattern(regexp = "^[a-zA-Z가-힣][a-zA-Z0-9가-힣]*$",
+            message = "아이템을 확인해 주세요.")
+    @Size(min = 2, max = 100, message = "상품명은 2-100자 사이어야 합니다")
+    private String item;
 }
 
 
