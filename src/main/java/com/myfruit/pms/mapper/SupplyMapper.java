90a55364f8e0d1c20730f33dc31580f6cc945003
shinis(ysit) <actionshin@gmail.com>
2025-01-23 14:20:16 +0900

    에러 처리 - @Mapper 누락 외 1건

--- src/main/java/com/myfruit/pms/mapper/SupplyMapper.java
+++ src/main/java/com/myfruit/pms/mapper/SupplyMapper.java
@@ -1,11 +1,13 @@
 package com.myfruit.pms.mapper;
 
 import com.myfruit.pms.dto.SupplyDto;
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 import java.util.Optional;
 
+@Mapper
 public interface SupplyMapper {
     void insertSupply(SupplyDto supplyDto); // PK가 생성
     Optional<SupplyDto> getSupplyById(int id);
