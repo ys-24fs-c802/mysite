37d761b986c6025c6a9db21c18c0f3303a66ee02
shinis(home) <actionshin@gmail.com>
2025-01-24 23:03:31 +0900

    [오류처리] 사업자등록번호 null이거나 중복을 허용하지 않도록 수정 - ''를 null로 변경

--- src/main/java/com/myfruit/pms/dto/SupplyDto.java
+++ src/main/java/com/myfruit/pms/dto/SupplyDto.java
@@ -4,11 +4,25 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Getter
-@Setter
 public class SupplyDto {
+    @Setter
     private Integer id;
+    @Setter
     private String name;
     private String contact1;
     private String contact2;
     private String businessNumber;
+
+    public void setContact1(String contact1) {
+        this.contact1 = contact1 != null && contact1.trim().isEmpty() ? null : contact1;
+    }
+
+    public void setContact2(String contact2) {
+        this.contact2 = contact2 != null && contact2.trim().isEmpty() ? null : contact2;
+    }
+
+    // unique
+    public void setBusinessNumber(String businessNumber) {
+        this.businessNumber = businessNumber != null && businessNumber.trim().isEmpty() ? null : businessNumber;
+    }
 }
